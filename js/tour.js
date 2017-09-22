3920fa498e8cd6498712404a832a33d86f4e56fa
akeshavan <anishakeshavan@gmail.com>
2017-09-22 14:48:27 -0700

    style: tour style

--- js/tour.js
+++ js/tour.js
@@ -5,7 +5,7 @@ function startIntro(){
            steps: [
              {
                intro: `<h1> Welcome! </h1>
-                <p> This tutorial will step through the features of Medulina </p>
+                <p> This tutorial will introduce the features of <strong>Medulina</strong> </p>
 
                `
              },
@@ -13,7 +13,7 @@ function startIntro(){
                //element: '#myCanvas',
                intro: `
 
-               <h1 style="text-align: center"> Draw and Fill </h1>
+               <h2 style="text-align: center"> Draw and Fill </h2>
 
                 <p> <strong> Click and Drag </strong> to draw a shape on the image </p>
 
@@ -23,18 +23,35 @@ function startIntro(){
                `
              },
              {
+               //element: '#myCanvas',
+               intro: `
+
+               <h2 style="text-align: center"> Zoom and Pan </h2>
+
+                <p> <strong> Scroll </strong>  or <strong> Pinch </strong> to zoom the image </p>
+
+                <p> <strong> Right-click + drag </strong> or <strong> Two finger drag </strong> to pan the image </p>
+
+
+               `
+             },
+             {
                element: '#hideshow',
-               intro: "Click here to hide/show your drawing",
+               intro: `Click here to <strong>hide</strong>/<strong>show</strong> your drawing`,
                position: 'top'
              },
              {
                element: '#menuButton',
-               intro: 'This menu gives you options to change your brush size, brush color, image birghtness and contrast',
+               intro: `This menu gives you options to
+               change your <strong>brush size</strong>,
+               <strong>brush color </strong>,
+               image <strong>brightness</strong>
+               and <strong>contrast</strong>`,
                position: 'top'
              },
              {
                element: '#undoButton',
-               intro: "Click to undo",
+               intro: "Click here to <strong>undo</strong>",
                position: 'top'
              },
              {
@@ -43,11 +60,16 @@ function startIntro(){
              }
            ]
          });
+         intro.onbeforechange(function(targetElement){
+           console.log("before change")
+           console.log("currentStep is", this._currentStep)
+
+         })
          intro.onafterchange(function(targetElement) {
             console.log("currentStep is", this._currentStep)
             //window.scrollTo(0,0)
 
-              if(this._currentStep == 2){
+              if(this._currentStep == 3){
                   overlay = document.getElementsByClassName("introjs-tooltipReferenceLayer");
                   console.log(overlay)
                   for(i=0; i<overlay.length; i++) {
@@ -61,7 +83,7 @@ function startIntro(){
                   }
               }
               //"introjs-fixedTooltip" is the other thing
-              if(this._currentStep == 3){
+              if(this._currentStep == 4){
                   overlay = document.getElementsByClassName("introjs-tooltipReferenceLayer");
                   console.log(overlay)
                   for(i=0; i<overlay.length; i++) {
@@ -71,7 +93,7 @@ function startIntro(){
 
                   }
               }
-              if(this._currentStep == 4){
+              if(this._currentStep == 5){
                   overlay = document.getElementsByClassName("introjs-tooltipReferenceLayer");
                   console.log(overlay)
                   for(i=0; i<overlay.length; i++) {
