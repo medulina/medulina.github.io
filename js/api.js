807b7a4ab10d0c0db1ee5ac8dc6814eae3ee5ff4
akeshavan <keshavan@berkeley.edu>
2017-09-28 14:10:01 -0700

    i thought i fixed this already

--- js/api.js
+++ js/api.js
@@ -151,11 +151,11 @@ do_save = function(score, edits){
     window.response = response;
     if (app.appMode == "train"){
       show_save(score)
-      roi.clear()
+      roi.remove()
       add_tp(response.tp)
       add_fp(response.fp)
-      add_fn(response.fn)
-      roi.insertAbove(fn)
+      add_fn(response.fn, 1)
+      //roi.insertAbove(fn)
 
       app.score.dice = response.score;
     }
